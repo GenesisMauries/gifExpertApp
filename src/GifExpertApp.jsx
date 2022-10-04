a6cadbb2514c0438093ac3a9468cda979f0ab125
GenesisMauries <exodo_54@hotmail.com>
2022-10-04 16:26:47 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+function GifExpertApp() {
+  return (
+    <>
+      {/* Titulo */}
+      <h1>Gif Expert APP</h1>
+      {/* Input / barra de busqueda */}
+      {/* Output / resultados */}
+    </>
+  );
+}
+
+export default GifExpertApp;
