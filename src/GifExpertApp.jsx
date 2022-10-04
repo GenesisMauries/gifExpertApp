2d821151738b0f1181c817557a4c6757367fbe6e
GenesisMauries <exodo_54@hotmail.com>
2022-10-04 16:49:38 -0500

    Cambia estado useState con el click de un boton

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
 function GifExpertApp() {
+  // mantener el estado de los elementos buscados
+  const [categories, setCategories] = useState(['Melendi', 'Miranda']);
+  // console.log(categories);
+  const onAddCategory = () => {
+    // Copia el estado y suma un nuevo elemento
+    setCategories([...categories, 'Dua Lipa ']);
+    //Usando callback de useState
+    // setCategories((cat) => [...cat, 'Dua Lipa']);
+  };
   return (
     <>
       {/* Titulo */}
       <h1>Gif Expert APP</h1>
+
       {/* Input / barra de busqueda */}
       {/* Output / resultados */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 }
