a4ccd5cdb6f8030216519c5b492aebc315b7a97f
GenesisMauries <exodo_54@hotmail.com>
2022-10-08 09:35:46 -0500

    Consulta en API de gifs sin hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,10 @@
+import { getGifs } from '../helpers/getGifs';
+
 export const GifGrid = ({ category }) => {
+  // Con esta invocacion se dispara la funcion cada que se renderiza el componente y se deberia llamar una sola vez
+  getGifs(category);
   return (
+    //
     <>
       <h3>{category}</h3>
     </>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+  const api_key = 'dCUj0zANniXUXF0JxtuxomLPEzqIiV4m';
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=25`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    };
+  });
+  console.log(gifs);
+  return gifs; // retorna el nuevo arreglo de gifs
+};
