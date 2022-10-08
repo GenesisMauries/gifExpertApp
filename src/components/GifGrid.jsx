567f40b9ca0045e9eea218f6a2aac0d22f075e19
GenesisMauries <exodo_54@hotmail.com>
2022-10-08 09:48:03 -0500

    useEffect para invocacion de funcion que consulta API

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,11 @@
+import { useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-  // Con esta invocacion se dispara la funcion cada que se renderiza el componente y se deberia llamar una sola vez
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
   return (
     //
     <>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -10,6 +10,6 @@ export const getGifs = async (category) => {
       url: img.images.downsized_medium.url,
     };
   });
-  console.log(gifs);
+  console.log(gifs); // se ejecuta dos veces por el stric mode de index.js
   return gifs; // retorna el nuevo arreglo de gifs
 };
